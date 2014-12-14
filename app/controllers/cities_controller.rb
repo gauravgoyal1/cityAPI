0b9a163c16890626413feb2af05efd291c00f7e4
Gaurav Goyal <gg.gauravgoyal@gmail.com>
2014-12-15 03:03:39 +0530

    modified response status codes

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -8,13 +8,13 @@ class CitiesController < ApplicationController
       @cities = City.where('country ILIKE ?', params[:country]).pluck(:name)
       if @cities.present?
         respond_to do |format|
-          format.html { render json: @cities, status: :found }
-          format.json { render json: @cities, status: :found }
+          format.html { render json: @cities, status: :ok }
+          format.json { render json: @cities, status: :ok }
         end
       else
         respond_to do |format|
-          format.html { render json: ["Error"], status: :error }
-          format.json { render json: ["Error"], status: :error }
+          format.html { render json: ["No Data"], status: :not_found }
+          format.json { render json: ["No Data"], status: :not_found }
         end
       end
     end
@@ -22,13 +22,13 @@ class CitiesController < ApplicationController
       @country = City.where('name ILIKE ?', params[:city]).pluck(:country)
       if @country.present?
         respond_to do |format|
-          format.html { render json: @country, status: :found }
-          format.json { render json: @country, status: :found }
+          format.html { render json: @country, status: :ok }
+          format.json { render json: @country, status: :ok }
         end
       else
         respond_to do |format|
-          format.html { render json: ["Error"], status: :error }
-          format.json { render json: ["Error"], status: :error }
+          format.html { render json: ["No Data"], status: :not_found }
+          format.json { render json: ["No Data"], status: :not_found }
         end
       end
     end
