008ee598d09d2dd24b2c5964c9d0859bd68dc5be
Gaurav Goyal <gg.gauravgoyal@gmail.com>
2014-12-15 16:16:56 +0530

    now api returns object rather than array

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,43 +1,54 @@
 class CitiesController < ApplicationController
 
   def index
+    if params[:countries].present?
+      countries = City.uniq.order('country ASC').pluck(:country)
+      if countries.present?
+        @json_data = {}
+        @json_data[:countries_count] = countries.count
+        @json_data[:countries] = countries
+        respond_to do |format|
+          format.html { render json: @json_data, status: :ok }
+          format.json { render json: @json_data, status: :ok }
+        end
+      end
+    end
+    
     if params[:country].present?
-      @cities = City.where('country ILIKE ?', params[:country]).order('name ASC').uniq.pluck(:name)
-      if @cities.present?
-        respond_to do |format|
-          format.html { render json: @cities, status: :ok }
-          format.json { render json: @cities, status: :ok }
-        end
+      cities = City.where('country ILIKE ? AND name IS NOT NULL', params[:country]).order('name ASC').uniq.pluck(:name)
+      if cities.present?
+        @json_data = {}
+        @json_data[:country] = params[:country]
+        @json_data[:cities_count] = cities.count
+        @json_data[:cities] = cities
       else
-        respond_to do |format|
-          format.html { render json: ["No Data"], status: :ok }
-          format.json { render json: ["No Data"], status: :ok }
-        end
+        @json_data = {}
+        @json_data[:country] = params[:country]
+        @json_data[:cities_count] = 0
+        @json_data[:cities] = ["No Data"]
+      end
+      respond_to do |format|
+        format.html { render json: @json_data, status: :ok }
+        format.json { render json: @json_data, status: :ok }
       end
     end
 
     if params[:city].present?
-      @country = City.where('name ILIKE ?', params[:city]).order('country ASC').pluck(:country)
-      if @country.present?
-        respond_to do |format|
-          format.html { render json: @country, status: :ok }
-          format.json { render json: @country, status: :ok }
-        end
+      country = City.where('name ILIKE ?', params[:city]).order('country ASC').uniq.pluck(:country)
+      if country.present?
+        @json_data = {}
+        @json_data[:city] = params[:city]
+        @json_data[:countries_count] = country.count
+        @json_data[:countries] = country
       else
-        respond_to do |format|
-          format.html { render json: ["No Data"], status: :ok }
-          format.json { render json: ["No Data"], status: :ok }
-        end
+        @json_data = {}
+        @json_data[:city] = params[:city]
+        @json_data[:countries_count] = 0
+        @json_data[:cities] = ["No Data"]
       end
-    end
-
-    if params[:countries].present?
-      @countries = City.uniq.order('country ASC').pluck(:country)
-      if @countries.present?
-        respond_to do |format|
-          format.html { render json: @countries, status: :ok }
-          format.json { render json: @countries, status: :ok }
-        end
+      respond_to do |format|
+        format.html { render json: @json_data, status: :ok }
+        format.json { render json: @json_data, status: :ok }
       end
     end
 
